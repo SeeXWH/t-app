c71c9f6187122dd9523154302b71520479efa94b
SeeXWH <mihaandre1@yamdex.ru>
2024-08-01 13:08:10 +0300

    main

---
+++ src/test/java/com/example/t_app/TAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.t_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
